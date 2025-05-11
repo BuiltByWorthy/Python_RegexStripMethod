3c5cb7e8c1e55be2301d2a39d644ee9fc0f7cede
BuiltByWorthy <worthyelevates@gmail.com>
2025-05-11 19:08:16 +0100

    Completed implementation.

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import re
 
 
-def regex_string(pattern: str = "") -> str:
-    return "hello"
+def regex_string(string: str, pattern: str = "") -> str:
+    if not pattern:
+        return re.sub(r"^\s+|\s+$", "", string)
+    # Preventing special characters from breaking regex.
+    escaped = re.escape(pattern)
+    return re.sub(fr"^[{escaped}]+|[{escaped}]+$", "", string)
 
 
-print(regex_string())
+ex1 = "     hello   "
+ex2 = "*****hello   *"
+ex3 = "  ***___hello ***____"
+ex4 = "==00==00 hello ==00"
+
+print(f"Before: |{ex1}| \nAfter: |{regex_string(ex1)}|\n")
+print(f"Before: |{ex2}| \nAfter: |{regex_string(ex2, "*")}|\n")
+print(f"Before: |{ex3}| \nAfter: |{regex_string(ex3, "_")}|\n")
+print(f"Before: |{ex4}| \nAfter: |{regex_string(ex4, "==00")}|\n")
