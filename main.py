253216e7575754d581c592c90def2bbf36124f6f
BuiltByWorthy <worthyelevates@gmail.com>
2025-05-11 19:15:27 +0100

    Giving the users the option for an exact match.

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import re
 
 
-def regex_string(string: str, pattern: str = "") -> str:
+def regex_string(string: str, pattern: str = "", exact: bool = False) -> str:
     if not pattern:
         return re.sub(r"^\s+|\s+$", "", string)
+
     # Preventing special characters from breaking regex.
     escaped = re.escape(pattern)
-    return re.sub(fr"^[{escaped}]+|[{escaped}]+$", "", string)
+
+    if not exact:
+        return re.sub(fr"^[{escaped}]+|[{escaped}]+$", "", string)
+    return re.sub(fr"^({escaped})+|({escaped})+$", "", string)
 
 
 ex1 = "     hello   "
 ex2 = "*****hello   *"
 ex3 = "  ***___hello ***____"
-ex4 = "==00==00 hello ==00"
+ex4 = "==00==00= hello ==00"
 
 print(f"Before: |{ex1}| \nAfter: |{regex_string(ex1)}|\n")
 print(f"Before: |{ex2}| \nAfter: |{regex_string(ex2, "*")}|\n")
 print(f"Before: |{ex3}| \nAfter: |{regex_string(ex3, "_")}|\n")
 print(f"Before: |{ex4}| \nAfter: |{regex_string(ex4, "==00")}|\n")
+print(f"Before: |{ex4}| \nAfter: |{regex_string(ex4, "==00", True)}|\n")
